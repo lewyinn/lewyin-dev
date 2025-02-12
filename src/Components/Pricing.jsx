d947fed1be34195e97ec4ce9e18a1319edd55c40
frenxyy <kurniawanridho500@gmail.com>
2025-02-12 09:43:36 +0700

    Push FullStack - FE

--- src/Components/Pricing.jsx
+++ src/Components/Pricing.jsx
@@ -1,109 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaCheckCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
-
-const pricingPlans = [
-    {
-        title: "Landing Page Basic",
-        category: 'coding',
-        price: "Rp. 750.000",
-        description: "Cocok untuk bisnis kecil yang ingin memiliki landing page profesional dengan desain modern dan responsif.",
-        features: [
-            "Free Domain .com 1 Tahun",
-            "Desain responsif & modern",
-            "Optimasi kecepatan & SEO dasar",
-            "Halaman statis informatif",
-            "Call Action To Whatsapp",
-            "Sertifikasi SSL untuk keamanan data",
-            "Instalasi Google Analytics untuk lacak pengunjung",
-            "Berisi maximal 3 Halaman Website",
-            "Memakai Hosting Gratis (Vercel)",
-        ],
-    },
-    {
-        title: "Landing Page Professional",
-        category: 'coding',
-        price: "Rp. 1.400.000",
-        description: "Ideal untuk bisnis yang berkembang dan membutuhkan landing page profesional dengan fitur yang lebih canggih untuk meningkatkan produktivitas.",
-        features: [
-            "Admin Panel Mengatur Content",
-            "Free Domain .com 1 Tahun",
-            "Shared Hosting 1 Tahun",
-            "Desain responsif & premium",
-            "Halaman dinamis dengan fitur tambahan",
-            "Optimasi SEO Dasar",
-            "Sertifikasi SSL untuk keamanan data",
-            "Instalasi Google Analytics untuk lacak pengunjung",
-            "Berisi maximal 4 Halaman Website",
-            "Memakai Shared Hosting",
-        ],
-    },
-    {
-        title: "Website Sekolah",
-        category: 'coding',
-        price: "Rp. 1.500.000",
-        description: "Dirancang untuk sekolah yang membutuhkan situs web profesional dengan fitur lengkap untuk mendukung Kontrol Penuh content website.",
-        features: [
-            "Admin Panel Kontrol Penuh Website",
-            "Free Domain .com 1 Tahun",
-            "Free Hosting 1 Tahun",
-            "Desain responsif & modern",
-            "Berisi maximal 5 Halaman Website",
-            "24/7 Dedicated Support",
-            "Sertifikasi SSL untuk keamanan data",
-            "Instalasi Google Analytics untuk lacak pengunjung",
-            "Memakai Shared Hosting",
-        ],
-    },
-    {
-        title: "Feed Instagram Basic",
-        category: 'desain',
-        price: "Rp. 250.000",
-        description: "Feed Instagram Basic membantu meningkatkan branding dengan 10 desain eksklusif, tampilan profesional, dan revisi gratis.",
-        features: [
-            "Proses cepat dan profesional",
-            "10 Desain Instagram berkualitas tinggi dan siap diposting",
-            "Desain eksklusif sesuai identitas brand Anda",
-            "Kombinasi warna dan font profesional untuk tampilan menarik",
-            "Revisi 5 kali gratis",
-            "Format file siap pakai (JPG, PNG, dan editable file jika diperlukan)",
-        ],
-    },
-    {
-        title: "Feed Instagram Professional",
-        category: 'desain',
-        price: "Rp. 600.000",
-        description: "Feed Instagram Basic membantu meningkatkan branding dengan 20 desain eksklusif, tampilan profesional, dan revisi gratis.",
-        features: [
-            "Proses cepat dan profesional",
-            "20 Desain Instagram berkualitas tinggi dan siap diposting",
-            "Desain eksklusif sesuai identitas brand Anda",
-            "Kombinasi warna dan font profesional untuk tampilan menarik",
-            "Revisi Unlimited Selama tidak merubah konsep",
-            "Format file siap pakai (JPG, PNG, dan editable file jika diperlukan)",
-        ],
-    },
-    {
-        title: "Banner Basic",
-        category: 'desain',
-        price: "Rp. 40.000",
-        description: "Banner Untuk Apapun itu dengan Desain yang Menarik dan Profesional",
-        features: [
-            "Proses cepat dan profesional",
-            "Banner Siap Cetak atau Siap Posting",
-            "Desain eksklusif sesuai identitas brand Anda",
-            "Kombinasi warna dan font profesional untuk tampilan menarik",
-            "Revisi Unlimited Selama tidak merubah konsep",
-            "Format file siap pakai (JPG, PNG, dan editable file jika diperlukan)",
-        ],
-    },
-];
+import { supabase } from '../SupabaseClient';
 
 export default function Pricing() {
-    const [category, setCategory] = useState("coding");
+    const [product, setProduct] = useState([]);
+    const [category, setCategory] = useState("website");
     const [expanded, setExpanded] = useState(false);
 
-    const filteredPlans = pricingPlans.filter(plan => plan.category === category);
+    useEffect(() => {
+        fetchProduct();
+    }, []);
+
+    async function fetchProduct() {
+        const { data, error } = await supabase.from("product").select("*");
+    
+        if (error) {
+            console.error("Error Fetching Data Product", error);
+        } else {
+            console.log("Data Fetched: ", data);
+            
+            // Konversi kolom 'feature' dari string JSON ke array
+            const processedData = data.map(product => ({
+                ...product,
+                feature: typeof product.feature === "string" 
+                    ? JSON.parse(product.feature || "[]") 
+                    : product.feature
+            }));
+    
+            setProduct(processedData);
+        }
+    }
+    
+
+    const filteredPlans = product.filter(plan => plan.category === category);
     const visiblePlans = expanded ? filteredPlans : filteredPlans.slice(0, 3);
 
     const toggleView = () => {
@@ -120,9 +50,9 @@ export default function Pricing() {
             </div>
             <div className="flex justify-center gap-4">
                 <button
-                    className={`px-6 py-2 border rounded-lg ${category === 'coding' ? 'bg-gray-700' : 'border-gray-700 hover:bg-gray-800'}`}
-                    onClick={() => setCategory("coding")}>
-                    Coding
+                    className={`px-6 py-2 border rounded-lg ${category === 'website' ? 'bg-gray-700' : 'border-gray-700 hover:bg-gray-800'}`}
+                    onClick={() => setCategory("website")}>
+                    Website
                 </button>
                 <button
                     className={`px-6 py-2 border rounded-lg ${category === 'desain' ? 'bg-gray-700' : 'border-gray-700 hover:bg-gray-800'}`}
@@ -141,13 +71,21 @@ export default function Pricing() {
                             <span className="relative group-hover:text-white">Hubungi Kami</span>
                         </Link>
                         <ul className="mt-4 text-left text-gray-300">
-                            {plan.features.map((feature, i) => (
-                                <li key={i} className="flex items-center text-sm gap-2 py-1">
-                                    <FaCheckCircle className="text-green-500" />
-                                    {feature}
-                                </li>
-                            ))}
+                            {Array.isArray(plan.feature)
+                                ? plan.feature.map((feature, i) => (
+                                    <li key={i} className="flex items-center text-sm gap-2 py-1">
+                                        <FaCheckCircle className="text-green-500" />
+                                        {feature}
+                                    </li>
+                                ))
+                                : JSON.parse(plan.feature || "[]").map((feature, i) => (
+                                    <li key={i} className="flex items-center text-sm gap-2 py-1">
+                                        <FaCheckCircle className="text-green-500" />
+                                        {feature}
+                                    </li>
+                                ))}
                         </ul>
+
                     </div>
                 ))}
             </div>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,28 +1,25 @@
-import React from 'react'
-import ProjectLandingElo from './../assets/RaftingLanding.png'
-import ProjectBarangApp from './../assets/pinjamApp.webp'
-import MediaTama from './../assets/MediaTama.png'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-
-const listProjects = [
-    {
-        judul: 'Laravel App - Sistem Pinjam Barang',
-        paragraf: 'Aplikasi berbasis Laravel untuk mempermudah manajemen peminjaman barang dengan fitur yang efisien dan user-friendly.',
-        image: ProjectBarangApp
-    },
-    {
-        judul: 'Landing Page - Rafting Magelang Elo & Progo',
-        paragraf: 'Website informatif dan menarik untuk pengalaman rafting seru di Sungai Elo & Progo, Magelang.',
-        image: ProjectLandingElo
-    },
-    {
-        judul: 'Landing Page - MediaTama',
-        paragraf: 'Website Portfolio mengenai MeditaTama sebagai Perusahaan penyedia layanan internet',
-        image: MediaTama
-    },
-]
+import { supabase } from './../SupabaseClient'
 
 const Projects = () => {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        fetchProjects();
+    }, []);
+
+    async function fetchProjects() {
+        const { data, error } = await supabase.from("projects").select("*");
+
+        if (error) {
+            console.error("Error fetching projects:", error);
+        } else {
+            console.log("Data fetched:", data);
+            setProjects(data || []);
+        }
+    }
+
     return (
         <>
             <div className="flex flex-col justify-center items-center text-white py-16 mx-auto text-center gap-8" id="project">
@@ -33,16 +30,16 @@ const Projects = () => {
                     </p>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                    {listProjects.map((item, index) => (
+                    {projects.map((item, index) => (
                         <div className='flex flex-col justify-center items-center rounded-4xl border-2 border-white/30' key={index}>
                             <img src={item.image} loading='lazy'
                                 alt="Lewyin Dev - Jasa pembuatan website profesional oleh Lewyin Dev" className='w-full h-auto lg:h-[314px] rounded-t-4xl object-cover' />
                             <div className='w-full h-auto lg:h-[240px] px-6 py-8 flex flex-col gap-2 rounded-b-4xl bg-gradient-to-br from-slate-700 to-slate-950'>
                                 <h2 className='text-xl font-semibold'>
-                                    {item.judul}
+                                    {item.title}
                                 </h2>
                                 <p className='text-gray-200 mb-4 h-auto lg:h-72'>
-                                    {item.paragraf}
+                                    {item.description}
                                 </p>
                                 <span className='flex justify-center items-center'>
                                     <Link to="https://wa.me/6285282932422" className="rounded-full px-5 py-2.5 overflow-hidden cursor-pointer group bg-blue-800 relative hover:bg-gradient-to-r hover:from-blue-800 hover:to-blue-600 text-white hover:ring-2 hover:ring-offset-2 hover:ring-blue-600 transition-all ease-out duration-300">
